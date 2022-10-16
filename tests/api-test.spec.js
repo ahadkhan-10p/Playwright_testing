87866918bc04e14c631e5f2fccbb77cd4986cf8a
ahadkhan-10p <ahad.khan@10pearls.com>
2022-10-16 19:38:34 +0500

    API Test Complete

--- tests/api-test.spec.js
+++ tests/api-test.spec.js
@@ -19,9 +19,50 @@ test.describe.serial("e2e Test", () => {
         })
         expect(response.status()).toBeTruthy()
         expect(response.status()).toBe(201)
+        console.log(await response.text())
         const responseJson = await response.json()
         id = await responseJson.id
         console.log(await id)
         
+    })
+
+    test("PUT - Update Whole User", async ({request}) => {
+        const response = await request.put("https://reqres.in/api/users/"+id, {
+            data : {
+                "name": "Ahad",
+                "job": "Tester"
+            }
+        })
+        expect(response.status()).toBeTruthy()
+        expect(response.status()).toBe(200)
+        console.log(await response.text())
+        const responseJson = await response.json()
+        const jobTitle = await responseJson.job
+        console.log(await jobTitle)
+        expect(jobTitle).toBe("Tester")
+        
+    })
+
+    test("PATCH - Update Single Field User", async ({request}) => {
+        const response = await request.patch("https://reqres.in/api/users/"+id, {
+            data : {
+                "name": "Ahad M Khan",
+                "job": "Tester"
+            }
+        })
+        expect(response.status()).toBeTruthy()
+        expect(response.status()).toBe(200)
+        console.log(await response.text())
+        const responseJson = await response.json()
+        const nameRes = await responseJson.name
+        console.log(await nameRes)
+        expect(nameRes).toBe("Ahad M Khan")        
+    })
+
+    test("Delete - Delete Created User", async ({request}) => {
+        const response = await request.patch("https://reqres.in/api/users/"+id)
+        expect(response.status()).toBeTruthy()
+        expect(response.status()).toBe(200)
+        console.log(await response.text())      
     })
 })
