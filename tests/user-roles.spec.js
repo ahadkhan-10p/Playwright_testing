1bd20f9606f4413bae446fe49dd00cfcba88a57a
ahadkhan-10p <ahad.khan@10pearls.com>
2022-10-16 19:23:23 +0500

    create call

--- tests/user-roles.spec.js
+++ tests/user-roles.spec.js
@@ -4,15 +4,23 @@ let id
 test.describe.serial("e2e Test", () => {
     
 
-    test("API Test", async ({ request }) => {
+    test("GET API Test", async ({ request }) => {
         const response = await request.get("https://reqres.in/api/users?page=2")
         expect(response.status()).toBeTruthy()
         expect(response.status()).toBe(200)
-        const responseJson = await response.json()
-        id = await responseJson.page
     })
 
-    test("Response print", async () => {
+    test("POST - Create User", async ({request}) => {
+        const response = await request.post("https://reqres.in/api/users", {
+            data : {
+                "name": "morpheus",
+                "job": "leader"
+            }
+        })
+        expect(response.status()).toBeTruthy()
+        expect(response.status()).toBe(201)
+        const responseJson = await response.json()
+        id = await responseJson.id
         console.log(await id)
         
     })
